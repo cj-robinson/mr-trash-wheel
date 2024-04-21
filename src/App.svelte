5c849f8cff714a04dbfdfa5ed0156962934503d4
CJ Robinson <cjcjcj777@gmail.com>
2024-04-21 19:35:53 -0400

    adds first pass at getting grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,18 @@
 <script>
-	import Visualization from './boat.svelte';
+	import Visualization from './collection.svelte';
     import Scrolly from "./Scrolly.svelte";
-
+	import Analysis from "./analysis.svelte";
 	let conclusionVisible = false;
     let value;
 
 	const steps = [
 		"<p>Welcome to the Jones Falls stream in Baltimore, Maryland. </p><p> Meet Mr. Trash Wheel®",
-		"<p>Mr. Trash Wheel® has a simple job. And that job is to consume garbage.",
-		"<p>Here comes some now.</p>",
+		"<p>Mr. Trash Wheel® has a simple job. And that job is to consume garbage.</p><p> He does that by using the water's current or his solar panels to rake through debris. It's pretty amazing to see.",
+		"<p>How much trash does a guy like this filter? Here comes garbage now.</p>",
 		"<p>This is one plastic bottle.</p>",
-		"<p>Here is a cigarette butt.</p>",
+		"<p>There goes a cigarette butt.</p>",
 		"<p>Anyone missing a ball? A glass bottle? Polyethylene? An old wrapper?</p>",
-		"<p>Let's see how much garbage comes through in a typical day.</p>"
+		"<p>Let's see how much comes through in a typical day's work, which we'll track as 24 hours.</p>"
 	];
 
 	function updateConclusionVisibility() {
@@ -49,11 +49,6 @@
 
 	.spacer {
 	  height: 100vh; /* Ensures that the visualization starts off-screen */
-	}
-  
-	.conclusion {
-	  transition: visibility 0.3s linear;
-	  visibility: hidden; /* Initially hidden, will be made visible dynamically */
 	}
 
 	.section-container {
@@ -104,16 +99,19 @@
 		flex: 1 1 0%;
 		z-index: 10;
 	}
-
-
+	.article{
+		background-color: #c8d9ed;
+	}
+	
 
   </style>
   
-  <article>
-	<section class="article-section">
-	  <h2>Garbage In, Garbage Out</h2>
+  <article class="article">
+	<section class="article-section" style= "text-align:center;">
+	  <h1>Garbage In, Garbage Out</h1>
+	  <br>
 	  <p>Society makes a lot of trash. We're not quite sure what to do with it.</p>
-	  <p>Anthropomorphizing might help.</p>
+	  <p>Anthropomorphizing might help. Powered by data from <a href = "https://www.mrtrashwheel.com/">www.mrtrashwheel.com</a>, and thanks to <a href="https://github.com/rfordatascience/tidytuesday/tree/master">#tidytuesday</a> for the idea. </p>
 	</section>
   
 	<!-- Spacer to ensure visualization starts off-screen -->
@@ -133,40 +131,8 @@
 		  <Visualization/>
 		</div>
 	  </div>
-
-	<!-- Additional spacer to ensure the visualization scrolls fully out of frame -->
-	<div class="spacer"></div>
-  
-	<section class="article-section conclusion" style="visibility: {conclusionVisible ? 'visible' : 'hidden'};">
-	  <h2>Conclusions Drawn from the Visualization</h2>
-	  <p>As we reflect on the visualization that has scrolled out of view, let's discuss the insights gathered. The movement and aggregation of circles within the defined area highlight the clustering tendency of data under certain conditions.</p>
-	  <p>This visualization is not just a showcase of technical prowess but serves as a metaphor for the dynamic and sometimes unpredictable nature of data analysis. Understanding these patterns allows us to anticipate changes and adapt strategies effectively.</p>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-	  <br>
-
-	</section>
+	<div style="position:sticky;">
+		<Analysis/>
+	</div>
   </article>
   
